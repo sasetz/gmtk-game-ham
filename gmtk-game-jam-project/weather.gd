15c9af41b66bb13de43218a5442e2f3341141e4d
Lev Subbota <71756643+GrimGutz@users.noreply.github.com>
2024-08-18 23:59:51 +0200

    погода обнова

--- gmtk-game-jam-project/weather.gd
+++ gmtk-game-jam-project/weather.gd
@@ -3,6 +3,7 @@ class_name Weather
 @export var type:String
 @export var background2:CompressedTexture2D=preload("res://Visual/Screens/Задник дождь или снег.png")
 @export var background1:CompressedTexture2D=preload("res://Visual/Backgrounds/Задник.png")
+@export var background3:CompressedTexture2D=preload("res://Visual/Screens/Задник красная луна.png")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	visible=false
@@ -13,8 +14,9 @@ func _ready():
 	pass # Replace with function body.
 
 func _new_weather_timeout():
+	self_modulate.a=1
 	match randi_range(1,9):
-		1,8,9:
+		1,9:
 			if not visible:
 				match randi_range(1,2):
 					1:
@@ -35,17 +37,40 @@ func _new_weather_timeout():
 			$"../UI/Background".texture=background1
 			$Weather_timer.wait_time=10.0
 			$Weather_timer.start()
-		5,4:
+		4:
 			type="Rain"
 			visible=true
 			play("Дождь")
 			$"../UI/Background".texture=background2
 			$Weather_timer.wait_time=5.0
 			$Weather_timer.start()
-		5,6,7:
+		5:
 			type="Snow"
 			visible=true
 			play("Снег")
+			$"../UI/Background".texture=background2
+			$Weather_timer.wait_time=5.0
+			$Weather_timer.start()
+		6:
+			type="Smoke"
+			visible=true
+			play("Туман")
+			self_modulate.a=0.8
+			$"../UI/Background".texture=background2
+			$Weather_timer.wait_time=5.0
+			$Weather_timer.start()
+		7:
+			type="Moon"
+			stop()
+			visible=true
+			$"../UI/Background".texture=background3
+			$Weather_timer.wait_time=5.0
+			$Weather_timer.start()
+		8:
+			type="Meteor"
+			print(type)
+			stop()
+			visible=true
 			$"../UI/Background".texture=background2
 			$Weather_timer.wait_time=5.0
 			$Weather_timer.start()
