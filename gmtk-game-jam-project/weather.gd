193a43acb0b6e8f96168d4a119af5f4ed2c57b8f
Lev Subbota <71756643+GrimGutz@users.noreply.github.com>
2024-08-18 23:31:08 +0200

    погода аним и этажи без кода

--- gmtk-game-jam-project/weather.gd
+++ gmtk-game-jam-project/weather.gd
@@ -1,6 +1,8 @@
 extends AnimatedSprite2D
 class_name Weather
 @export var type:String
+@export var background2:CompressedTexture2D=preload("res://Visual/Screens/Задник дождь или снег.png")
+@export var background1:CompressedTexture2D=preload("res://Visual/Backgrounds/Задник.png")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	visible=false
@@ -12,7 +14,7 @@ func _ready():
 
 func _new_weather_timeout():
 	match randi_range(1,9):
-		1:
+		1,8,9:
 			if not visible:
 				match randi_range(1,2):
 					1:
@@ -22,27 +24,29 @@ func _new_weather_timeout():
 						type="Wind1"
 						flip_h=false
 			visible=true
-			play("default")
+			play("Ветер")
+			$"../UI/Background".texture=background1
 			$Weather_timer.wait_time=5.0
 			$Weather_timer.start()
-		2,3,4:
+		2,3:
 			stop()
 			type="None"
 			visible=false
+			$"../UI/Background".texture=background1
 			$Weather_timer.wait_time=10.0
 			$Weather_timer.start()
-		5:
+		5,4:
 			type="Rain"
 			visible=true
-			print("RAIN")
-			play("default")
+			play("Дождь")
+			$"../UI/Background".texture=background2
 			$Weather_timer.wait_time=5.0
 			$Weather_timer.start()
-		5,6,7,8,9:
+		5,6,7:
 			type="Snow"
 			visible=true
-			print("SNOW")
-			play("default")
+			play("Снег")
+			$"../UI/Background".texture=background2
 			$Weather_timer.wait_time=5.0
 			$Weather_timer.start()
 			
