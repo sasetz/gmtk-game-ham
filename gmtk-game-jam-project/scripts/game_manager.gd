ceda21809c7637be6ba27a8b228692977f84392a
Kirill Putiatin <36602508+sasetz@users.noreply.github.com>
2024-08-18 18:35:28 +0200

    Reworked inventory system to always have all slots at hand

--- gmtk-game-jam-project/scripts/game_manager.gd
+++ gmtk-game-jam-project/scripts/game_manager.gd
@@ -2,32 +2,9 @@ extends Node2D
 
 @onready var inventory := $Inventory
 
-var _inventory_giver_timer: Timer
-@onready var _item_roster := [
-	inventory.CUBE_ITEM,
-	inventory.RAMP_ITEM,
-	inventory.CIRCLE_ITEM,
-	inventory.ROTATE_ITEM,
-	inventory.SQUARE_ITEM,
-	inventory.RESIZE_ITEM,
-	inventory.DELETE_ITEM,
-]
-var _next_roster_index := 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_inventory_giver_timer = Timer.new()
-	add_child(_inventory_giver_timer)
-	_inventory_giver_timer.wait_time = 3
-	_inventory_giver_timer.one_shot = false
-	_inventory_giver_timer.start()
-	_inventory_giver_timer.timeout.connect(func():
-		inventory.add_item(_item_roster[_next_roster_index])
-		_next_roster_index += 1
-		_next_roster_index %= _item_roster.size() # restricts the index to only this array's size
-	)
-	inventory.add_item(inventory.RAMP_ITEM)
-	inventory.add_item(inventory.CUBE_ITEM)
+	pass
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_released("MWU"):
